4bdc00d1ce257d4ff034417bfa5e9e81ef53fea2
olia0973469 <Olha.Tykha.RI.2023@lpnu.ua>
2023-09-28 12:38:34 +0300

    message

---
+++ Project4/Source.cpp
@@ -0,0 +1,30 @@
+// Lab_02.cpp
+// Тиха Ольга Северинівна
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 0.28
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x; // вхідний параметр
+	double y; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	z1 = (cos(x) * cos(x) * cos(x) * cos(x)) + (sin(y) * sin(y)) + (1. / 4) * (sin(2 * x) * sin(2 * x)) - 1;
+	z2 = sin(y+x) * sin(y-x);
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
